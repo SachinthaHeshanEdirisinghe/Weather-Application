62de9a7f38ba26761b3a0c357e2a5fe54ebe5518
SachinthaHeshanEdirisinghe <SachinthaHeshanEdirisinghe@gmail.com>
2025-11-16 16:58:28 +0530

    fetch API & edit ui

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,5 @@
 console.log("Weather app is running");
+
+fetch("http://api.weatherapi.com/v1/current.json?key=0efbc67303144d118d495821251611    &q=Colombo&aqi=no")
+.then(res => res.json())
+.then(data => console.log(data));
